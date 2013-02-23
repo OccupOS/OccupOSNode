f227aaafc5f4f1e2b3c0d1c2f5b7ed71edd41217
Morrison Cole <admin@morrisoncole.co.uk>
2013-02-23 16:13:39 +0000

    Added (unofficial) Azure Mobile port for .NET Micro and added Gadgeteer project.

--- OccupOSNode.Micro.Gadgeteer/Properties/AssemblyInfo.cs
+++ OccupOSNode.Micro.Gadgeteer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OccupOSNode.Micro")]
-[assembly: AssemblyDescription("The software for the OccupOS micro nodes")]
+[assembly: AssemblyTitle("OccupOSNode.Micro.Gadgeteer")]
+[assembly: AssemblyDescription("The software for the OccupOS Gadgeteer micro nodes")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OccupOS")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("GadgeteerDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,5 +21,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
