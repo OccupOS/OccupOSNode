258a85ea537c7b39d081f980478f8d67f3c7722a
Jisang Choi <7517choi@armymail.mod.uk>
2013-03-03 15:24:13 +0000

    commit attempt final time

---
+++ OccupOSNode.Micro.Netduino/NetworkControllers/Arduino/ArduinoWirelessNetworkController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Text;
+using Microsoft.SPOT;
+using Toolbox.NETMF;
+using Toolbox.NETMF.Hardware;
+using Toolbox.NETMF.NET;
+
+namespace OccupOSNode.Micro.NetworkControllers.Arduino {
+    public class ArduinoWirelessNetworkController : NetworkController {
+        private String address;
+        private SimpleSocket socket;
+
+        public ArduinoWirelessNetworkController(String hostName, int port) {
+
+            WiFlyGSX WifiModule = new WiFlyGSX();
+            WifiModule.EnableDHCP();
+            WifiModule.JoinNetwork(hostName);
+
+            SimpleSocket socket = new WiFlySocket(hostName, 80, WifiModule);
+            socket.connect();
+
+        }
+
+        public int sendData(string data) {
+
+            byte[] buffer = Encoding.UTF8.GetBytes(data);
+            return socket.Send(buffer);
+        }
+
+        public void disconnect() {
+            socket.Close();
+        }
+
+    }
+}
--- OccupOSNode.Micro.Netduino/NetworkControllers/Arduino/ArduinoWirelessShieldController.cs
+++
@@ -1,3 +0,0 @@
-namespace OccupOSNode.Micro.NetworkControllers.Arduino {
-    internal class ArduinoWirelessShieldController : OccupOS.CommonLibrary.NetworkControllers.NetworkController { }
-}
