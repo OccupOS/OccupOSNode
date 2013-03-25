1aaef4776ee441b1a30f6e30a273edb7dbffd2d0
Morrison Cole <admin@morrisoncole.co.uk>
2013-03-25 17:30:46 +0000

    Began updating SQL helper to interface with latest Azure designs

--- OccupOS.Common/PacketFactory.cs
+++ OccupOS.Common/PacketFactory.cs
@@ -1,3 +1,23 @@
 namespace OccupOS.CommonLibrary {
-    internal class PacketFactory { }
+    using OccupOS.CommonLibrary.Sensors;
+
+    internal class PacketFactory
+    {
+        public string CreatePacket(SensorData sensorData)
+        {
+            string packet;
+            packet = packet.AddCommaSeperatedValue(System.DateTime.Now.ToString());
+            packet = packet.AddCommaSeperatedValue(sensorData.AnalogLight.ToString());
+
+            return packet;
+            // PolledAt, SensorType (see skydrive JSON schema 1.1), ID, IntermediateHwMedadataId, MeasuredData).
+        }
+    }
+
+    internal static class StringExtension {
+        public static string AddCommaSeperatedValue(this string str, string value)
+        {
+            return str.Length == 0 ? value : str + "," + value;
+        }
+    }
 }
